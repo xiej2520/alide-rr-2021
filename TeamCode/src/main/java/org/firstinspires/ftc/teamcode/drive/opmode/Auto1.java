d86e4341ab02bb47de67c4e376b06b252cc16ce6
Dylan Topolovec <dctopolovec2@gmail.com>
2021-03-05 15:40:44 -0500

    Improved auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Auto1.java
@@ -18,63 +18,69 @@ public class Auto1 extends LinearOpMode {
 
     private FtcDashboard dashboard;
 
-    public static Pose2d start = new Pose2d(-60, 48, Math.toRadians(180));
-    public static Pose2d zoneA = new Pose2d(12.0, 60.0, Math.toRadians(180));
-    public static Pose2d launchPos = new Pose2d(0.0, 36.0, Math.toRadians(180));
-    public static Pose2d launchPos2 = new Pose2d(0.0, 36.0, Math.toRadians(0));
-    public static Pose2d rings = new Pose2d(-36, 36, Math.toRadians(0));
-    public static Pose2d launchLine = new Pose2d(12, 36, Math.toRadians(0));
+    public static Pose2d startP = new Pose2d(-60, -48, Math.toRadians(180));
+    public static Pose2d launchPosP = new Pose2d(0.0, -36.0, Math.toRadians(180));
 
-    public static Vector2d startV = new Vector2d(-60, 48);
-    public static Vector2d zoneAV = new Vector2d(12.0, 60.0);
-    public static Vector2d launchPosV = new Vector2d(0.0, 36.0);
-    public static Vector2d ringsV = new Vector2d(-36, 36);
-    public static Vector2d launchLineV = new Vector2d(12, 36);
+    public static Vector2d zoneAV = new Vector2d(12.0, -60.0);
+    public static Vector2d middleStepV = new Vector2d(-24, -60.0);
+    public static Vector2d launchPosV = new Vector2d(0.0, -36.0);
+    public static Vector2d ringsV = new Vector2d(-36, -36);
+    public static Vector2d launchLineV = new Vector2d(12, -36);
 
-    public static double shooterAngle = .3;
-    public static double vel = -1200;
+    public static double shooterAngle = 35;
+    public static double vel = -2000;
 
-    public static int shootCount = 5;
-    public static int shootWait = 1000;
+    public static int shootCount = 3;
+    public static int shootWait = 750;
 
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Robot roboto = new Robot(hardwareMap);
 
-        Pose2d startPose = new Pose2d(-60, 48, Math.toRadians(180));
+        Pose2d startPose = new Pose2d(-60, -48, Math.toRadians(180));
         drive.setPoseEstimate(startPose);
 
         dashboard = roboto.drive.dashboard;
 
-        Trajectory startToZoneA = drive.trajectoryBuilder(start) // moves around rings and then positions on zone A so the wobble goal can be delivered
-                .splineToConstantHeading(zoneAV, Math.toRadians(180))
+        Trajectory startToZoneA1 = drive.trajectoryBuilder(startP)
+                .lineToConstantHeading(middleStepV)
                 .build();
 
-        Trajectory zoneAToLaunchPos = drive.trajectoryBuilder(zoneA) // moves around rings and then positions on zone A so the wobble goal can be delivered
-                .splineToLinearHeading(launchPos, Math.toRadians(0))
+        Trajectory startToZoneA2 = drive.trajectoryBuilder(startToZoneA1.end())
+                .lineToConstantHeading(zoneAV)
                 .build();
 
-        Trajectory launchToRing = drive.trajectoryBuilder(launchPos2) // move to rings to pick up
+        Trajectory zoneAToLaunchPos = drive.trajectoryBuilder(startToZoneA2.end())
+                .splineToLinearHeading(launchPosP, Math.toRadians(0))
+                .build();
+
+        Trajectory launchToRing = drive.trajectoryBuilder(zoneAToLaunchPos.end()
+                .plus(new Pose2d(0, 0, Math.toRadians(180)))) // robot turns before this traj
                 .splineToConstantHeading(ringsV, Math.toRadians(180))
                 .build();
 
-        Trajectory ringsToLaunchPos = drive.trajectoryBuilder(rings) // move to launch position
+        Trajectory ringsToLaunchPos = drive.trajectoryBuilder(launchToRing.end())
                 .splineToConstantHeading(launchPosV, Math.toRadians(0))
                 .build();
 
-        Trajectory launchPosToLaunchLine = drive.trajectoryBuilder(launchPos2) // move to launch line
+        Trajectory launchPosToLaunchLine = drive.trajectoryBuilder(ringsToLaunchPos.end())
                 .splineToConstantHeading(launchLineV, Math.toRadians(0))
                 .build();
 
         waitForStart();
 
-        drive.followTrajectory(startToZoneA);
+        drive.followTrajectory(startToZoneA1);
+        drive.followTrajectory(startToZoneA2);
 
-        // open wobble goal method
+        roboto.setShooterAngleDeg(roboto.shooterAngleMaxDeg);
+        sleep(500);
+        roboto.setWobbleGrabberMode(false);
+        sleep(500);
 
         drive.followTrajectory(zoneAToLaunchPos);
 
+        sleep(1000);
         drive.turn(Math.toRadians(180));
 
         // shoot 5x
@@ -91,6 +97,8 @@ public class Auto1 extends LinearOpMode {
         drive.turn(Math.toRadians(180));
 
         roboto.setIntakeMode(true);
+
+        sleep(5000); // stopping so I can debug
 
         drive.followTrajectory(launchToRing);
 
