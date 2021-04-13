931c984f955658087013c43ba60cbb307df57e79
Jacky Xie <jackyxie2520@outlook.com>
2021-04-13 15:47:06 -0400

    got EasyOpenCV demo working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/EasyOpenCVTest.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.dashboard.config.Config;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.Robot;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Config
+@Autonomous(group = "drive")
+
+public class EasyOpenCVTest extends LinearOpMode{
+
+    private FtcDashboard dashboard;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+        dashboard = FtcDashboard.getInstance();
+        dashboard.setTelemetryTransmissionInterval(25);
+
+        Robot roboto = new Robot(hardwareMap);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId",
+                "id", hardwareMap.appContext.getPackageName()
+        );
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened(){
+                camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+            }
+        });
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+
+        }
+
+
+    }
+}
